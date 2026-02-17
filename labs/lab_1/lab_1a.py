21180f41e665fd706de57c9d090fbb7fa37a307f
Intelexia <98366953+Intelexia@users.noreply.github.com>
2026-02-17 10:23:26 -0800

    Add introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Max Yang" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi all!\nI'm Max, a sophomore from San Jose, California. In my free time, I enjoy solving fun physics problems (specifically mechanics) and coding video games! I'm looking forward to learning more about git and how to use it to manage codebases through this course!")
 if __name__ == "__main__":
     main()
